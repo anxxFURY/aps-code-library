e9d4867d56753fa27f1ad1634d120a34cb05b53d
anxxFURY <anihadagali.official@gmail.com>
2023-02-13 19:09:13 +0530

    changes made

---
+++ lazy-propogation.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "algorithm"
+#include "vector"
+#include "list"
+#include "utility"
+#include "unordered_map"
+using namespace std;
+
+#define ll long long int
+#define prlong long int(n) cout << n;
+#define loop(n) for(long long int i = 0; i < n; i++)
+#define rloop(n) for(long long int j = n-1; j>0; j--)
+
+using namespace std;
+
+void solve() {
+    int x,y;
+    cin >> x >> y;
+}
+int main() {
+    // your code goes here
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    long long int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+}
