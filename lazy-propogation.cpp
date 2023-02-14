221526d6fcab8659faf5886296f62cebc6e7ed1a
anxxFURY <anihadagali.official@gmail.com>
2023-02-15 02:29:43 +0530

    changing lazy-prop

--- lazy-propogation.cpp
+++ lazy-propogation.cpp
@@ -13,9 +13,36 @@ using namespace std;
 
 using namespace std;
 
+// array sum in given range:
+
+class ST {
+    vector<int> st , lazy;
+public:
+    ST(int n) {
+        st.resize(4*n),lazy.resize(4*n);
+    }
+
+    void build(int ind, int low, int high, int a[]) {
+        if(low == high) {
+            st[ind] =   a[low];
+            return ;
+        }
+
+        int mid = (low + high) / 2;
+
+        build(2*ind+1, low, mid, a);
+        build(2*ind+2,mid+1,high,a);
+        st[ind] = st[2*ind+1] + st[2*ind+2];
+    }
+
+    void update(int ind,int low, int high,int l, int r, int val) {
+
+    }
+};
+
+
 void solve() {
-    int x,y;
-    cin >> x >> y;
+
 }
 int main() {
     // your code goes here
